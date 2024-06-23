b38497e15b00fe005c921cd55cb679c76b3ffdce
praghur <166060957+praghur@users.noreply.github.com>
2024-06-23 15:33:47 -0400

    Added Node 2

--- profile.py
+++ profile.py
@@ -131,11 +131,16 @@ node1 = request.RawPC("node1")
 node1.hardware_type = params.nodetype
 node1.disk_image = UBUNTU_IMG
 
+node2 = request.RawPC("node2")
+node2.hardware_type = params.nodetype
+node2.disk_image = UBUNTU_IMG
+
 for srs_type, type_hash in DEFAULT_SRS_HASHES.items():
     cmd = "{} '{}' {}".format(SRS_DEPLOY_SCRIPT, type_hash, srs_type)
     node1.addService(rspec.Execute(shell="bash", command=cmd))
 
 node1.addService(rspec.Execute(shell="bash", command=OPEN5GS_DEPLOY_SCRIPT))
+node2.addService(rspec.Execute(shell="bash", command=SRS_DEPLOY_SCRIPT))
 
 tour = IG.Tour()
 tour.Description(IG.Tour.MARKDOWN, tourDescription)
