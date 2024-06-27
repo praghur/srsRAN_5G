23b92a47d6894dc4214ba49084659bb22e5fb43a
praghur <166060957+praghur@users.noreply.github.com>
2024-06-27 08:36:47 -0400

    Update NATforward.sh

--- NATforward.sh
+++ NATforward.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # nat port forwarding 
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o enp6s0f1 -j MASQUERADE
 sudo systemctl stop ufw
 sudo iptables -I FORWARD 1 -j ACCEPT
+
